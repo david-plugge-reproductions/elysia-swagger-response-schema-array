7cc724ffa7d5ad8179364b2cf6dad6d3a0a5ef86
David Plugge <git@davidplugge.de>
2023-09-15 17:58:49 +0200

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from 'elysia';
+import { swagger } from '@elysiajs/swagger';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const Post = t.Object({
+    title: t.String(),
+});
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia()
+    .use(swagger())
+    .get('/', () => 'Hello Elysia')
+    .post(
+        '/posts',
+        ({ body }) => {
+            return body;
+        },
+        {
+            type: 'json',
+            body: t.Array(Post),
+            response: {
+                200: t.Array(Post),
+            },
+        },
+    )
+    .listen(3000);
+
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
+console.log(`🦊 Docs can be found at http://${app.server?.hostname}:${app.server?.port}/swagger`);
